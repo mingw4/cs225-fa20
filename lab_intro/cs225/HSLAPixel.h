8eb7ab229a6139d3fbf379e3fb19e8f99005cf96
Ming Wang <Ming@Mings-MacBook-Pro.local>
2020-09-01 06:37:14 -0500

    Finished implementing HSLAPixel.

--- lab_intro/cs225/HSLAPixel.h
+++ lab_intro/cs225/HSLAPixel.h
@@ -11,4 +11,24 @@
 #include <sstream>
 
 namespace cs225 {
-}
+    class HSLAPixel {
+        public:
+            //h denotes the hue of the pixel [0,360)
+            double h;
+            //s denotes the saturation of the pixel [0,1]
+            double s;
+            //l denotes the luminance of the pixel, [0,1]
+            double l;
+            //a denotes the alpha chanel, namely transparancy, [0,1]
+            double a;
+            HSLAPixel();
+            HSLAPixel(double hue,
+            double saturation,
+            double luminance);
+            HSLAPixel(double hue,
+            double saturation,
+            double luminane,
+            double alpha);
+    };
+};
+
