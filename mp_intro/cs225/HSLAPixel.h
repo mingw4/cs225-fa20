8d931c6962b1b31973062b335a71a87d78a23789
Ming Wang <Ming@Mings-MacBook-Pro.local>
2020-09-04 17:33:16 -0500

    First

--- mp_intro/cs225/HSLAPixel.h
+++ mp_intro/cs225/HSLAPixel.h
@@ -2,9 +2,33 @@
  * @file HSLAPixel.h
  *
  * @author CS 225: Data Structures
+ * @version 2018r1-lab1
  */
 
 #pragma once
 
+#include <iostream>
+#include <sstream>
+
 namespace cs225 {
-}
+    class HSLAPixel {
+        public:
+            //h denotes the hue of the pixel [0,360)
+            double h;
+            //s denotes the saturation of the pixel [0,1]
+            double s;
+            //l denotes the luminance of the pixel, [0,1]
+            double l;
+            //a denotes the alpha chanel, namely transparancy, [0,1]
+            double a;
+            HSLAPixel();
+            HSLAPixel(double hue,
+            double saturation,
+            double luminance);
+            HSLAPixel(double hue,
+            double saturation,
+            double luminane,
+            double alpha);
+    };
+};
+
