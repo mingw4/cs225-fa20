59309cffe1e15b9ab15ae88f530098ee4e550b75
Ming Wang <Ming@Mings-MacBook-Pro.local>
2020-09-07 16:58:51 -0500

    First

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -5,7 +5,23 @@
 
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+  // TODO: Part 2.
+  cs225::PNG* original = new cs225::PNG();
+  original->readFromFile(inputFile);
+  unsigned width = original->width();
+  unsigned height = original->height();
+
+  cs225::PNG* output = new cs225::PNG(width, height);
+  
+  for (unsigned y = 0; y < height; y++) {
+    for (unsigned x = 0; x < width; x++) {
+      cs225::HSLAPixel thePixel = (*original).getPixel(x, y);
+      (*output).getPixel(width - x, height - y) = thePixel;
+    }
+  }
+  output->writeToFile(outputFile);
+  delete output;
+  delete original;
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
