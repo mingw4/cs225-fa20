07447047fc4a7f79851e5cb67c4f8131d61cc7ed
Ming Wang <Ming@Mings-MacBook-Pro.local>
2020-09-08 13:04:07 -0500

    Done

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -2,6 +2,7 @@
 #include "cs225/HSLAPixel.h"
 
 #include <string>
+#include <cmath>
 
 
 void rotate(std::string inputFile, std::string outputFile) {
@@ -27,6 +28,13 @@ void rotate(std::string inputFile, std::string outputFile) {
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
   // TODO: Part 3
-
+  for (unsigned y = 0; y < height; y++) {
+    for (unsigned x = 0; x < width; x++) {
+      png.getPixel(x,y).h = 160 * sin(x / 255) + 180;
+      png.getPixel(x,y).s = 0.2 * (cos(x + 3) + sin(y)) + 0.5;
+      png.getPixel(x,y).l = 0.15 * (cos(x - y) - sin(y - 20)) + 0.5;
+      png.getPixel(x,y).a = 0.1 * (cos(x) + sin(y + x)) + 0.5;
+    }
+  }
   return png;
 }
--- mp_intro/intro.h
+++ mp_intro/intro.h
@@ -3,6 +3,7 @@
 #include "cs225/PNG.h"
 
 #include <string>
+#include <cmath>
 
 void rotate(std::string inputFile, std::string outputFile);
 cs225::PNG myArt(unsigned int width, unsigned int height);
