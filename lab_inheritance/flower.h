e1f85615872c7308ef3116798444f2525196ff5a
mingw4 <mingw4@illinois.edu>
2020-09-17 16:59:48 -0500

    Done?

--- lab_inheritance/flower.h
+++ lab_inheritance/flower.h
@@ -6,6 +6,11 @@
 #pragma once
 
 #include "shape.h"
+#include "circle.h"
+#include "drawable.h"
+#include "rectangle.h"
+#include "vector2.h"
+#include "triangle.h"
 
 /**
  * A subclass of Drawable that can draw a flower
@@ -13,9 +18,11 @@
 class Flower : public Drawable
 {
   private:
-    Shape stem;
-    Shape pistil; // center piece of flower
-    Shape leaf;
+    Vector2 stem_center;
+    Vector2 leaf_center;
+    Rectangle* stem;
+    Circle* pistil; // center piece of flower
+    Triangle* leaf;
 
     void drawPetals(cs225::PNG* canvas, const Vector2& center, int x, int y) const;
 
