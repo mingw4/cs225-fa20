4ba1a37b3ea1f68ed33d8e982455b5ccb00c222b
mingw4 <mingw4@illinois.edu>
2020-09-20 05:42:35 -0500

    MP2 ALL done?

--- mp_stickers/main.cpp
+++ mp_stickers/main.cpp
@@ -9,13 +9,11 @@ int main() {
   //
     Image alma;     alma.readFromFile("tests/alma.png");
     Image i;        i.readFromFile("tests/i.png");
-    Image f;        f.readFromFile("tests/soln_flower.png");
-    Image c;        c.readFromFile("tests/soln_test_constuctor.png");
     StickerSheet sheet(alma, 5);
     sheet.addSticker(i, 40, 20);
-    sheet.addSticker(f, 200, 80);
-    sheet.addSticker(c, 400, 200);
-    sheet.addSticker(f, 600, 400);
-    (sheet.render()).writeToFile("MyImage.png");
+    sheet.addSticker(i, 200, 80);
+    sheet.addSticker(i, 400, 200);
+    sheet.addSticker(i, 600, 400);
+    (sheet.render()).writeToFile("myImage.png");
     return 0;
 }
