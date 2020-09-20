84643b86491c3421be3b6f5ca24e1814a69341b0
mingw4 <mingw4@illinois.edu>
2020-09-20 05:37:31 -0500

    MP 2 Part III half-way

--- mp_stickers/main.cpp
+++ mp_stickers/main.cpp
@@ -7,6 +7,15 @@ int main() {
   // Reminder:
   //   Before exiting main, save your creation to disk as myImage.png
   //
-
-  return 0;
+    Image alma;     alma.readFromFile("tests/alma.png");
+    Image i;        i.readFromFile("tests/i.png");
+    Image f;        f.readFromFile("tests/soln_flower.png");
+    Image c;        c.readFromFile("tests/soln_test_constuctor.png");
+    StickerSheet sheet(alma, 5);
+    sheet.addSticker(i, 40, 20);
+    sheet.addSticker(f, 200, 80);
+    sheet.addSticker(c, 400, 200);
+    sheet.addSticker(f, 600, 400);
+    (sheet.render()).writeToFile("MyImage.png");
+    return 0;
 }
