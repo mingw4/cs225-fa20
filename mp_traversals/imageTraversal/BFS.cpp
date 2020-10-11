177d253e4d66b5125a50332eb58903a43e74a673
mingw4 <mingw4@illinois.edu>
2020-10-11 12:35:34 -0500

    BFS

--- mp_traversals/imageTraversal/BFS.cpp
+++ mp_traversals/imageTraversal/BFS.cpp
@@ -24,6 +24,10 @@ using namespace cs225;
  */
 BFS::BFS(const PNG & png, const Point & start, double tolerance) {  
   /** @todo [Part 1] */
+  png_ = png;
+  start_ = start;
+  tolerance_ = tolerance;
+  queue_.push(start_);
 }
 
 /**
@@ -47,6 +51,7 @@ ImageTraversal::Iterator BFS::end() {
  */
 void BFS::add(const Point & point) {
   /** @todo [Part 1] */
+  queue_.push(point);
 }
 
 /**
@@ -54,7 +59,9 @@ void BFS::add(const Point & point) {
  */
 Point BFS::pop() {
   /** @todo [Part 1] */
-  return Point(0, 0);
+  Point temp = queue_.front();
+  queue_.pop();
+  return temp;
 }
 
 /**
@@ -62,7 +69,7 @@ Point BFS::pop() {
  */
 Point BFS::peek() const {
   /** @todo [Part 1] */
-  return Point(0, 0);
+  return queue_.front();
 }
 
 /**
@@ -70,5 +77,5 @@ Point BFS::peek() const {
  */
 bool BFS::empty() const {
   /** @todo [Part 1] */
-  return true;
+  return queue_.empty();
 }
--- mp_traversals/imageTraversal/BFS.h
+++ mp_traversals/imageTraversal/BFS.h
@@ -35,4 +35,8 @@ public:
 private:
   /** @todo [Part 1] */
   /** add private members here*/
+  std::queue<Point> queue_;
+  PNG png_;
+  Point start_;
+  double tolerance_;
 };
