a1dfad6a09e5c145075ccade151f100342749d22
mingw4 <mingw4@illinois.edu>
2020-10-17 21:40:52 -0500

    first on lab

--- lab_avl/avltree.cpp
+++ lab_avl/avltree.cpp
@@ -30,6 +30,17 @@ void AVLTree<K, V>::rotateLeft(Node*& t)
 {
     functionCalls.push_back("rotateLeft"); // Stores the rotation name (don't remove this)
     // your code here
+    Node*& r1 = t;
+    Node * p1 = t;
+    Node*& r2 = t->right;
+    Node * p2 = t->right;
+    Node*& r3 = t->right->left;
+    Node * p3 = t->right->left;
+    r1 = p2;
+    r2 = p3;
+    r3 = p1;
+    p1->height = std::max(heightOrNeg1(p1->left), heightOrNeg1(p1->right)) + 1;
+    p2->height = std::max(heightOrNeg1(p2->left), heightOrNeg1(p2->right)) + 1;
 }
 
 template <class K, class V>
@@ -46,6 +57,17 @@ void AVLTree<K, V>::rotateRight(Node*& t)
 {
     functionCalls.push_back("rotateRight"); // Stores the rotation name (don't remove this)
     // your code here
+    Node*& r1 = t;
+    Node * p1 = t;
+    Node*& r2 = t->left;
+    Node * p2 = t->left;
+    Node*& r3 = t->left->right;
+    Node * p3 = t->left->right;
+    r1 = p2;
+    r2 = p3;
+    r3 = p1;
+    p1->height = std::max(heightOrNeg1(p1->left), heightOrNeg1(p1->right)) + 1;
+    p2->height = std::max(heightOrNeg1(p2->left), heightOrNeg1(p2->right)) + 1;
 }
 
 template <class K, class V>
@@ -53,12 +75,31 @@ void AVLTree<K, V>::rotateRightLeft(Node*& t)
 {
     functionCalls.push_back("rotateRightLeft"); // Stores the rotation name (don't remove this)
     // your code here
+    rotateRight(t->right);
+    rotateLeft(t);
 }
 
 template <class K, class V>
 void AVLTree<K, V>::rebalance(Node*& subtree)
 {
     // your code here
+    int b = heightOrNeg1(subtree->right) - heightOrNeg1(subtree->left);
+    if (b == -2) {
+        int lb = heightOrNeg1(subtree->left->right) - height(subtree->left->left);
+        if (lb == -1) {
+            rotateRight(subtree);
+        } else {
+            rotateLeftRight(subtree);
+        }
+    } else if (balance == 2) {
+        int rb = heightOrNeg1(subtree->right->right) - heightOrNeg1(subtree->left->left);
+        if (rb == 1) {
+            rotateLeft(subtree);
+        } else {
+            rotateRightLeft(subtree);
+        }
+    }
+    subtree->height = std::max(heightOrNeg1(subtree->left), heightOrNeg1(subtree->right)) + 1;
 }
 
 template <class K, class V>
@@ -71,6 +112,14 @@ template <class K, class V>
 void AVLTree<K, V>::insert(Node*& subtree, const K& key, const V& value)
 {
     // your code here
+    if (subtree == NULL) {
+        subtree = new Node(key, value);
+    } else if (key < subtree->key) {
+        insert(subtree->left, key, value);
+    } else if (key > subtree->key) {
+        insert(subtree->right, key, value);
+    }
+    rebalance(subtree);
 }
 
 template <class K, class V>
@@ -87,19 +136,39 @@ void AVLTree<K, V>::remove(Node*& subtree, const K& key)
 
     if (key < subtree->key) {
         // your code here
+        remove(subtree->left, key);
     } else if (key > subtree->key) {
         // your code here
+        remove(subtree->right, key);
     } else {
         if (subtree->left == NULL && subtree->right == NULL) {
             /* no-child remove */
             // your code here
+            delete subtree;
+            subtree = NULL;
         } else if (subtree->left != NULL && subtree->right != NULL) {
             /* two-child remove */
             // your code here
+            Node * curr = subtree->left;
+            while (curr->right != NULL) {
+                curr = curr->left;
+            }
+            subtree->key = curr->key;
+            remove(subtree->left, curr->key);
         } else {
             /* one-child remove */
             // your code here
+            if (subtree->left != NULL) {
+                Node * temp = subtree->left;
+                delete subtree;
+                subtree = temp;
+            } else {
+                Node * temp = subtree->right;
+                delete subtree;
+                subtree = temp;
+            }
         }
         // your code here
+        rebalance(subtree);
     }
 }
