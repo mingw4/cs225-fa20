7c538e3853fac5efc08f154154107f0c42913e3f
mingw4 <mingw4@illinois.edu>
2020-11-02 10:30:50 -0600

    ..

--- mp_mosaics/maptiles.cpp
+++ mp_mosaics/maptiles.cpp
@@ -21,7 +21,23 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
     /**
      * @todo Implement this function!
      */
-
-    return NULL;
+    size_t j = 0;
+    size_t size = theTiles.size();
+    vector<Point<3>> vec;
+    map<Point<3>, size_t> mp;
+    MosaicCanvas* mc = new MosaicCanvas(theSource.getRows(), theSource.getColumns());
+    while (j < size) {
+        mp[convertToXYZ(theTiles[j].getAverageColor())] = j;
+        vec.push_back(convertToXYZ(theTiles[j].getAverageColor()));
+        ++j;
+    }
+    KDTree<3> t(vec);
+    for (size_t k = 0; k < theSource.getColumns(); ++k) {
+        for (size_t l = 0; l < theSource.getRows(); ++l) {
+            TileImage* tile = &theTiles[mp[mp.find(t.findNearestNeighbor(convertToXYZ(theSource.getRegionColor(l, k))))]];
+            mc->setTile(l, k, tile);
+        }
+    }
+    return mc;
 }
 
