d023f78500c30432e7275a182138acedb9bd46d6
mingw4 <mingw4@illinois.edu>
2020-11-15 19:48:47 -0600

    can travel done

--- mp_mazes/maze.cpp
+++ mp_mazes/maze.cpp
@@ -6,16 +6,81 @@
 #include "maze.h"
 
 SquareMaze::SquareMaze() {
-
+    width_ = 0;
+    height_ = 0;
+    maze_ = new std::vector<std::pair<bool, bool>>;
+    path_ = new DisjointSets();
 }
+
+
+SquareMaze::~SquareMaze() {
+    delete maze_;
+    delete path_;
+    maze_ = NULL;
+    path_ = NULL;
+}
+
 
 void SquareMaze::makeMaze(int width, int height) {
-    
+    delete maze_;
+    delete path_;
+    width_ = width;
+    height_ = height;
+    maze_ = new std::vector<std::pair<bool, bool>>;
+    path_ = new DisjointSets();
+    for (int j = 0; j < width_ * height_; ++j) {
+        maze_->push_back(std::make_pair(true, true));
+    }
+    path_->addelements(width_ * height_);
 }
 
+
+
+
 bool SquareMaze::canTravel(int x, int y, int dir) const {
-    return true;
+    if (3 == dir) {
+        if (1 > y) {
+            return false;
+        } else if ((*maze_)[(y - 1) * width_ + x].first) {
+            return false;
+        } else {
+            return true;
+        }
+    } else if (2 == dir) {
+        if (1 > x) {
+            return false;
+        } else if ((*maze_)[y * width_ + x - 1].second) {
+            return false;
+        } else {
+            return true;
+        }
+    } else if (1 == dir) {
+        if (height_ - 2 < y) {
+            return false;
+        } else if ((*maze_)[y * width_ + x].first) {
+            return false;
+        } else {
+            return true;
+        }
+    } else if (0 == dir) {
+        if (width_ - 2 < x) {
+            return false;
+        } else if ((*maze_)[y * width_ + x].second) {
+            return false;
+        } return true;
+    } else {
+        return false;
+    }
+
+
 }
+
+
+
+
+
+
+
 
 void SquareMaze::setWall(int x, int y, int dir, bool exists) {
 
--- mp_mazes/maze.h
+++ mp_mazes/maze.h
@@ -11,10 +11,18 @@
 class SquareMaze {
     public:
         SquareMaze();
+        ~SquareMaze();
         void makeMaze(int width, int height);
         bool canTravel(int x, int y, int dir) const;
         void setWall(int x, int y, int dir, bool exists);
         std::vector<int> solveMaze();
         cs225::PNG* drawMaze() const;
         cs225::PNG* drawMazeWithSolution();
+
+    private:
+        int width_;
+        int height_;
+        //first bool is downwards.
+        std::vector<std::pair<bool, bool>> * maze_;
+        DisjointSets * path_;
 };
